c67a8cb88a1f68aa300b0ea94faf6b86f35ef77e
dermen <dermen@lbl.gov>
2020-04-08 11:30:52 -0700

    udpate

--- FormatHDF5AttributeGeometry.py
+++ FormatHDF5AttributeGeometry.py
@@ -1,17 +1,24 @@
 from __future__ import absolute_import, division
 
 import numpy as np
-import ast
 import h5py
+import json
+from copy import deepcopy
 
 from dxtbx.format.FormatHDF5 import FormatHDF5
 from dials.array_family import flex
 from dxtbx.format.FormatStill import FormatStill
+try:
+    from dxtbx.model import SpectrumBeam
+    HAS_SPECTRUM_BEAM = True
+except ImportError:
+    HAS_SPECTRUM_BEAM = False
 
 
 class FormatHDF5AttributeGeometry(FormatHDF5, FormatStill):
     """
-    Class for reading D9114 simulated monolithic cspad data
+    Class for reading HDF5 files for arbitrary geometries
+    focused on performance
     """
     @staticmethod
     def understand(image_file):
@@ -39,12 +46,39 @@ class FormatHDF5AttributeGeometry(FormatHDF5, FormatStill):
         self._handle = h5py.File(image_file, "r")
         self._image_dset = self._handle["images"]
         self._geometry_define()
+        self._has_spectra = False
+        self._has_central_wavelengths = False 
+        self._energies = None
+        self._weights = None
+        self._central_wavelengths = None
+        self._check_per_shot_spectra()
+        self._ENERGY_CONV = 12398.419739640716
 
     def _geometry_define(self):
         det_str = self._image_dset.attrs["dxtbx_detector_string"]
         beam_str = self._image_dset.attrs["dxtbx_beam_string"]
-        self._cctbx_detector = self._detector_factory.from_dict(ast.literal_eval(det_str))
-        self._cctbx_beam = self._beam_factory.from_dict(ast.literal_eval(beam_str))
+        try:
+            det_str = det_str.decode()
+            beam_str = beam_str.decode()
+        except AttributeError:
+            pass
+        det_dict = json.loads(det_str)
+        beam_dict = json.loads(beam_str)
+        self._cctbx_detector = self._detector_factory.from_dict(det_dict)
+        self._cctbx_beam = self._beam_factory.from_dict(beam_dict)
+
+    def _check_per_shot_spectra(self):
+        keys = list(self._handle.keys())
+        has_energies = "spectrum_energies" in keys
+        has_weights = "spectrum_weights" in keys
+        has_central_wavelengths = "central_wavelengths" in keys
+        if has_energies and has_weights:
+            self._has_spectra = True
+            self._energies = self._handle["spectrum_energies"]
+            self._weights = self._handle["spectrum_weights"]
+        elif has_central_wavelengths:
+            self._has_central_wavelengths = True
+            self._central_wavelengths = self._handle["central_wavelengths"]
 
     def get_num_images(self):
         return self._image_dset.shape[0]
@@ -63,8 +97,31 @@ class FormatHDF5AttributeGeometry(FormatHDF5, FormatStill):
     def get_detector(self, index=None):
         return self._cctbx_detector
 
-    def get_beam(self, index=None):
-        return self._cctbx_beam
+    def _get_wavelength(self, index):
+        if self._has_spectra:
+            w = self._weights[index]
+            E = self._energies[index]
+            ave_E = (w*E).sum() / (w.sum())
+            wavelength = self._ENERGY_CONV / ave_E 
+            if HAS_SPECTRUM_BEAM:
+                self._w = w
+                self._E = E
+        elif self._has_central_wavelengths:
+            wavelength = self._central_wavelengths[index]
+        else:
+            wavelength = None
+        return wavelength
+
+    def get_beam(self, index=0):
+        beam = self._cctbx_beam
+        wavelength = self._get_wavelength(index)
+        if wavelength is not None: 
+            beam = deepcopy(self._cctbx_beam)
+            beam.set_wavelength(wavelength)
+        if HAS_SPECTRUM_BEAM and self._has_spectra:
+            beam = SpectrumBeam.from_dict(beam.to_dict())
+            beam.set_spectrum(self._E, self._w)
+        return beam
 
 
 if __name__ == '__main__':
